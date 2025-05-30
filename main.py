4ea7855b156fbf05bbf20a53d9814c45e15385d9
CHRISTOPHER CHAPARRO CASTILLO <127803678+Chris6264@users.noreply.github.com>
2025-05-29 21:55:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import os
-from preprocessing import load_and_preprocess_fer2013, visualize_feature_extraction_example
+from data_preprocessing import load_and_preprocess_fer2013, visualize_feature_extraction_example
 from model_training import train_and_evaluate_model
 from realtime_detector import run_realtime_detector
 
-# Ruta del dataset y el modelo
 FER2013_DATA_PATH = 'fer2013.csv'
 MODEL_SAVE_PATH = 'models/emotion_detector_cnn.keras'
 
 def main():
-
     print("--- Inicio ---")
 
-    # 1. Preprocesamiento de Datos y Division
     print("\n## 1. Procesando y dividiendo el dataset FER-2013.")
     X_train, X_test, X_val, y_train, y_test, y_val, num_classes, emotion_labels = \
         load_and_preprocess_fer2013(FER2013_DATA_PATH)
@@ -20,23 +17,19 @@ def main():
         print("Error: No se pudo cargar el dataset.")
         return
 
-    print("\nVisualizando un ejemplo de extraccion de caracteristicas")
-    sample_image_for_viz = X_train[0].squeeze() * 255 
+    print("\nVisualizando un ejemplo de extracción de características.")
+    sample_image_for_viz = X_train[0].squeeze() * 255
     visualize_feature_extraction_example(sample_image_for_viz)
 
-
-    # 2. Entrenamiento y Evaluacion del Modelo
     print("\n---")
     print("## 2. Entrenando y evaluando el modelo CNN.")
     train_and_evaluate_model(X_train, y_train, X_val, y_val, X_test, y_test, num_classes, MODEL_SAVE_PATH)
 
-
-    # 3. Deteccion de Emociones en Tiempo Real
     print("\n---")
     print("## 3. Iniciando el detector de emociones en tiempo real.")
     run_realtime_detector(MODEL_SAVE_PATH, emotion_labels)
 
-    print("\n--- Fin. ---")
+    print("\n--- Pipeline completado. ---")
 
 if __name__ == '__main__':
     main()
